cc09d5505b92117f1a8d130525f48a5401691f8f
Anton Lamtev <antonlamtev@gmail.com>
2017-01-19 23:10:44 +0300

    Update build.sh

--- java/build.sh
+++ java/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t lamtev/java .
-docker run lamtev/java /bin/bash -c " java -version && javac -version && gradle --version "
+docker run lamtev/java /bin/bash -c " java -version && javac -version && gradle -version && mvn -version "

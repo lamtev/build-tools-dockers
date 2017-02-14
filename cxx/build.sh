1756f9697ae9347fb32adc86e36624e0ec2734c7
Anton Lamtev <antonlamtev@gmail.com>
2017-02-15 00:40:08 +0300

    Update build.sh

--- cxx/build.sh
+++ cxx/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t lamtev/cxx .
-docker run lamtev/cxx /bin/bash -c " gcc --version && g++ --version && cloc --version && cppcheck --version && valgrind --version && cmake --version "
+docker run lamtev/cxx /bin/bash -c " gcc --version && g++ --version && cloc --version && cppcheck --version && valgrind --version && cmake --version && doxygen -version "
